92c98b1c7fdf797a8da154b74ac6e7d29b4546c3
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-24 03:47:12 +0000

    automatically backed up by learn

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -13,7 +13,7 @@ class Rating extends React.Component {
 
   componentWillReceiveProp(nextProp){
   this.setState({
-    increasing: this.props.rating < nextProp.rating,
+    increasing: nextProp.rating > this.props.rating,
     decreasing: this.props.rating > nextProp.rating
   })
   }
